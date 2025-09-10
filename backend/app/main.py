d7c36873bfefca30aab190d374e53fc8a15d2a92
sukharevssss-pixel <sukharevssss@gmail.com>
2025-09-10 22:40:50 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from . import db
+import db
 
 app = FastAPI(title="TG MiniApp Backend")
 
